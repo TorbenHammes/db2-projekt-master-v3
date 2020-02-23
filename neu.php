2f461a3e8daae0a68812a8e10358e76f59e85696
Torben Hammes <torben@hammes.media>
2020-02-23 17:11:36 +0100

    Update

--- neu.php
+++ neu.php
@@ -1,2 +1,25 @@
+<?php
+$dblink=new mysqli('localhost','hammes','mango','hammes');
+?>
+
 <html>
-</html>
+<head>
+<meta http-equiv="refresh" content="0; URL=produktliste.php">
+</head>
+<?php
+
+
+
+$produktname=strip_tags($_GET['produktname']);
+$v_nickname=strip_tags($_GET['v_nickname']);
+$preis=strip_tags($_GET['preis']);
+$kategorie=strip_tags($_GET['kategorie']);
+$beschreibung=strip_tags($_GET['beschreibung']);
+$bild=strip_tags($_GET['bild']);
+
+
+$ins="insert into TBL_Verkaeufer_Produkt (produktname,v_nickname,preis,kategorie,beschreibung,bild) values('$produktname','$v_nickname','$preis','$kategorie','$beschreibung','$bild')";
+
+$dblink->query($ins);
+
+?>
